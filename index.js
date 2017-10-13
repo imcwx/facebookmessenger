922e17fccf7d54649cf3cb163e9c916ec00df341
wenxiang <chewwenxiang@gmail.com>
2017-10-13 15:31:49 +0800

    change response

--- index.js
+++ index.js
@@ -88,9 +88,14 @@ function receivedMessage(event) {
 
   if (messageText) {
 
+    // apiaiRequest.on('response', (response) => {
+    // var aiText = response.result.fulfillment.speech;
+    // console.log(aiText);
+
     apiaiRequest.on('response', (response) => {
-    var aiText = response.result.fulfillment.speech;
+    var aiText = response;
     console.log(aiText);
+
 
     // If we receive a text message, check to see if it matches a keyword
     // and send back the example. Otherwise, just echo the text we received.
